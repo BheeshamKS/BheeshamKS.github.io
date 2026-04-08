58fede04f54a11dbd06825a91cbf9e7079945e7b
BheeshamKS <bheeshamkumarsajnani3@gmail.com>
2026-04-08 18:03:09 +0500

    FIX

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,18 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom')) {
+            return 'vendor';
+          }
+          if (id.includes('node_modules/framer-motion')) {
+            return 'motion';
+          }
+        }
+      }
+    }
+  }
 })
